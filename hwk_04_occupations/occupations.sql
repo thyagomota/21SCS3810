f4194cd044306166110b5c97cd7fc299cb3834a8
Thyago Mota <tmota@msudenver.edu>
2021-02-07 09:06:47 -0700

    updates

---
+++ hwk_04_occupations/occupations.sql
@@ -0,0 +1,28 @@
+-- occupations database
+-- created at: <date>
+-- author: <your name>
+
+CREATE DATABASE occupations;
+
+USE occupations;
+
+DROP TABLE IF EXISTS Occupations;
+
+-- TODO: create table Occupations
+
+
+-- TODO: populate table Occupations
+
+-- TODO: a) the total number of occupations (expect 1016).
+
+-- TODO: b) a list of all job families in alphabetical order (expect 23).
+
+-- TODO: c) the total number of job families (expect 23)
+
+-- TODO: d) the total number of occupations per job family in alphabetical order of job family.
+
+-- TODO: e) the number of occupations in the "Computer and Mathematical" job family (expect 38)
+
+-- TODO: f) an alphabetical list of occupations in the "Computer and Mathematical" job family.
+
+-- TODO: g) an alphabetical list of occupations in the "Computer and Mathematical" job family that begins with the word "Database"
