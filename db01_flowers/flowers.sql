380008f5eaf72412dda7e3b273abdfebb1b7d393
Thyago Mota <tmota@msudenver.edu>
2021-02-25 13:27:31 -0700

    updates

---
+++ db01_flowers/flowers.sql
@@ -0,0 +1,29 @@
+-- flowers database
+-- created at: <date>
+-- author(s): <your name(s)>
+
+-- database creation and use
+
+-- tables creation satisfying all of the requirements
+
+-- tables population
+
+-- a) the total number of zones.
+
+-- b) the number of flowers per cool zone.
+
+-- c) common names of the plants that have delivery sizes less than 5.
+
+-- d) common names of the plants that require full sun (i.e., sun needs contains ‘S’).
+
+-- e) all delivery category names order alphabetically (without repetition).
+
+-- f) the exact output (see instructions)
+
+-- g) plant names that have the same hot zone as “Pink Caladiums” (your solution MUST get the hot zone of “Pink Caladiums” in a variable).
+
+-- h) the total number of plants, the minimum delivery size, the maximum delivery size, and the average size based on the plants that have delivery sizes (note that the average value should be rounded using two decimals).
+
+-- i) the Latin name of the plant that has the word ‘Eyed’ in its name (you must use LIKE in this query to get full credit).  
+
+-- j) the exact output (see instructions)
