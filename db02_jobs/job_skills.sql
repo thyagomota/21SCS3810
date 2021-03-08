2bca212fbc42764f8bce92227e4e08b313afbf11
Thyago Mota <tmota@msudenver.edu>
2021-03-08 15:06:01 -0700

    updates

---
+++ db02_jobs/job_skills.sql
@@ -0,0 +1,24 @@
+-- CS3810 - Principles of Database Systems - Spring 2021
+-- Instructor: Thyago Mota
+-- Description: job_skills database script
+-- Student(s) Name(s):
+
+-- database creation and use
+
+-- tables creation
+
+-- data access
+
+-- queries
+
+-- a) what is the total number of job positions?
+
+-- b) what is the total number of skills?
+
+-- c) which job position titles have the word "database"?
+
+-- d) provide an alphabetical list of all job position titles that require "sql" or "mysql" as a skill.
+
+-- e) which skills "database analyst" like positions have that "database admin" like positions don't?
+
+-- f) list the top 20 skills required by job positions having the word "database" in their titles.
---
+++ db02_jobs/src/JobSkills.java
@@ -0,0 +1,27 @@
+/*
+ * CS3810 - Principles of Database Systems - Spring 2021
+ * Instructor: Thyago Mota
+ * Description: DB 02 - JobSkills
+ * Student(s) Name(s):
+ */
+
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+
+public class JobSkills {
+
+    public static String DATASET = "data/job_skills.csv";
+
+    public static void main(String[] args) throws IOException, SQLException {
+
+        // TODO: load database properties
+
+
+        // TODO: connect to the database
+
+
+        // TODO: complete the data load
+        
+    }
+}
